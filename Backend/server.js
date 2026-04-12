ace1b2cb5ad0a0da6757a765d2a70b3e88c889f7
Aryan Gupta <aryan.gupta3_cs23@gla.ac.in>
2026-04-12 19:30:54 +0530

    [Aryan Gupta ] fix: express 5 wildcard route for production

--- Backend/server.js
+++ Backend/server.js
@@ -60,7 +60,7 @@ const __dirname = path.dirname(__filename);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../Frontend-job-portal-client-side/dist")));
 
-  app.get("*", (req, res) =>
+  app.get("/*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "../", "Frontend-job-portal-client-side", "dist", "index.html"))
   );
 }
